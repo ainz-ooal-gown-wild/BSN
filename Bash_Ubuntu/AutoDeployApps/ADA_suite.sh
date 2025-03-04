c90741960419aa1450502b9ca02635fa9d407bbc
isweardownonmynansgrave <weber.lukas2@outlook.de>
2025-03-04 13:22:22 +0100

    Rework

---
+++ Bash_Ubuntu/AutoDeployApps/ADA_suite.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Relative paths
+CONF="conf/example_singleFile.conf"
+EXMOD="./modules/"
+# Logging settings here?
+
+# Module deployment here
+# Selection by deleting comment #
+#$EXMOD+"install_apache2.sh"
